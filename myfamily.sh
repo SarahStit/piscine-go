c027eecbcf6328a565baaae3c865e6c6a781748b
SarahStit <sarah.stito@gmail.com>
2019-09-27 16:59:23 +0100

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
-#curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json| jq -r '. [0].name'
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq .[${HERO_ID}-1] | grep relatives | sed 's/ "relatives": \"//g'| sed 's/"//g'
+#curl -s https://raw.githubusercontent.com/kigiri/supecrhero-api/master/api/all.json| jq -r '. [0].name'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq .[${HERO_ID}-1] | grep relatives | sed 's/    "relatives": \"//'g | sed 's/"//g'
